0c44a72f90529cde2edae7ac17cb1620d8b58c2b
opstech23 <bbody8215@gmail.com>
2017-11-11 15:01:55 +0800

    changed context of belongd_to relationship in migration files

--- db/migrate/20171111064904_create_project_tasks.rb
+++ db/migrate/20171111064904_create_project_tasks.rb
@@ -1,12 +1,12 @@
 class CreateProjectTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :project_tasks do |t|
-      t.belongs_to :project_id, foreign_key: true
-      t.belongs_to :user_id, foreign_key: true
+      t.belongs_to :project, index: true,foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true
       t.string :task_title
       t.string :task_description
-      t.belongs_to :approved_by_admin, foreign_key: true
-      t.belongs_to :approved_by_pto, foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps
     end
